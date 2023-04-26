eb0bf27de508c336e56c196718ec5b9237187e05
Jack <jackmurphy756@gmail.com>
2023-04-26 15:10:38 +0100

    Implement Repl

--- Parser.hs
+++ Parser.hs
@@ -3,14 +3,17 @@ module Main where
 import Control.Monad.Except
 import Data.Functor
 import System.Environment
+import System.IO
 import Text.ParserCombinators.Parsec hiding (spaces)
 import Text.Read.Lex (Number)
 
 main :: IO ()
 main = do
   args <- getArgs
-  let evaled = fmap show $ readExpr (head args) >>= eval
-  putStrLn $ extractValue $ trapError evaled
+  case length args of
+    0 -> runRepl
+    1 -> evalAndPrint $ head args
+    _ -> putStrLn "This repl expects one or fewer arguments"
 
 readExpr :: String -> ThrowsError LispVal
 readExpr str = case parse parseExpr "lisp" str of
@@ -261,3 +264,25 @@ parseQuoted = do
   char '\''
   x <- parseExpr
   return $ List [Atom "quote", x]
+
+flushStr :: String -> IO ()
+flushStr str = putStr str >> hFlush stdout
+
+readPrompt :: String -> IO String
+readPrompt prompt = flushStr prompt >> getLine
+
+evalString :: String -> IO String
+evalString expr = return $ extractValue $ trapError (fmap show $ readExpr expr >>= eval)
+
+evalAndPrint :: String -> IO ()
+evalAndPrint expr = evalString expr >>= putStrLn
+
+until_ :: Monad m => (a -> Bool) -> m a -> (a -> m ()) -> m ()
+until_ pred prompt action = do
+  result <- prompt
+  if pred result
+    then return ()
+    else action result >> until_ pred prompt action
+
+runRepl :: IO ()
+runRepl = until_ (== "quit") (readPrompt "Lisp >>> ") evalAndPrint
